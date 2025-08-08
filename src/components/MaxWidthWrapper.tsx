0bf89103879e12c4579886f3f29c8b8c1911f36d
Sina <sinaghodsi93@gmail.com>
2025-08-08 22:14:40 +0330

    style(MaxWidthWrapper): update max-width to 1400px for improved layout consistency

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -11,7 +11,7 @@ const MaxWidthWrapper = ({
   return (
     <div
       className={cn(
-        "mx-auto w-full max-w-8xl px-4 md:px-20 lg:px-24",
+        "mx-auto w-full max-w-[1400px] px-4 md:px-20 lg:px-24",
         className
       )}
     >
